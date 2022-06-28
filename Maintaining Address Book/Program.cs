0041c4a91f454bd7e44f9d364d7a8a1bd68d1b7c
soni240 <s.fegade21@gmail.com>
2022-06-28 23:09:10 +0530

    [Gunavanti]Add:Adding Data in Address Book

--- Maintaining Address Book/Program.cs
+++ Maintaining Address Book/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using System;
 using MaintainingAddressBook;
 public class Program
@@ -7,38 +6,18 @@ public class Program
     public static void Main(String[] args)
     {
         Console.WriteLine("********** Welcome in this Address Book ************");
-        AddressBook addressBook = new AddressBook();
+        Contact addressBook = new Contact();
         Console.WriteLine("Please Enter the Information");
-        bool check = true;
-        Console.WriteLine(" Enter the Zero to Stop The Execution 0\n Display The Information1\n Edit the Information 2\n Delete The Information 3\n Add Data in the Dictionary 4\n EditingDictionary Data in the AddressBook");
+        addressBook.FirstName = Console.ReadLine();
+        addressBook.LastName = Console.ReadLine();
+        addressBook.Address = Console.ReadLine();
+        addressBook.City = Console.ReadLine();
+        addressBook.State = Console.ReadLine();
+        addressBook.EmailAddress = Console.ReadLine();
+        addressBook.PostalCode = Convert.ToInt32(Console.ReadLine());
+        addressBook.MobileNumber = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(addressBook.FirstName + " " + addressBook.LastName + " " + addressBook.Address + " " + addressBook.City + " " + addressBook.State + " " + addressBook.EmailAddress + " " + addressBook.PostalCode + " " + addressBook.MobileNumber);
 
-        while (check)
-        {
-            Console.WriteLine("***** Please Enter The Above Option *******");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
-            {
-                case 1: addressBook.Display(); break;
-                case 2:
-                    Console.WriteLine("Enter the Name for Edit the Information");
-                    string name = Console.ReadLine();
-                    addressBook.EditContactInAddressBook(name); break;
-                case 3:
-                    Console.WriteLine("Enter the Name for Delete the Information");
-                    string name1 = Console.ReadLine();
-                    addressBook.DeletingContactINAddressBook(name1); break;
-                case 4:
-                    Console.WriteLine("Enter the Name for Adding data in Dictionary ");
-                    string dictionaryName = Console.ReadLine();
-                    addressBook.AddDictionary(dictionaryName); break;
-                case 5:
-                    Console.WriteLine("Enter the Name for Adding data in Dictionary ");
-                    string dictName = Console.ReadLine();
-                    string contactName = Console.ReadLine();
-                    addressBook.EditingDictionary(dictName, contactName); break;
-                case 0: check = false; break;
-            }
-
-        }
     }
 }
+
